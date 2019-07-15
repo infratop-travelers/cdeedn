988c837dfb78eaacf258741f5064bb6daa50fa7f
kmgk <kamegaki.shuji.infratop@gmail.com>
2019-07-15 19:50:16 +0900

    create singer model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_15_104809) do
+ActiveRecord::Schema.define(version: 2019_07_15_104922) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "post_code", null: false
@@ -85,6 +85,12 @@ ActiveRecord::Schema.define(version: 2019_07_15_104809) do
     t.integer "postage", null: false
     t.string "address", null: false
     t.string "payment", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "singers", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
