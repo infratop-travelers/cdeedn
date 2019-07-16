9e8e35357f22baedd378d24e734d83e17c5026ca
kmgk <kamegaki.shuji.infratop@gmail.com>
2019-07-16 19:48:38 +0900

    管理者_ユーザー一覧画面の作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Customer.create(email: "test1@test1", last_name: "test1last", read_last_name: "テスト１姓", first_name: "test1name", read_first_name: "テスト１名", phone_number: "testphonenumber", post_code: "test_post_code", prefecture: "test_prefecture", city: "test-city", street: "test-street", password: "testtest")
