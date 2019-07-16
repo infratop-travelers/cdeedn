16bfe6b31195ce16945d88b09b9b082f4b0da722
HAGARIHAYATO <hagari.hayato.infratop@gmail.com>
2019-07-16 18:19:34 +0900

    items_controller modified

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,14 @@
 class ItemsController < ApplicationController
   def index
+  	@items = Item.all
   end
 
   def show
+  	@item = Item.find(params[:id])
+  end
+
+  private
+  def item_params
+  	params.require(:item).permit(:image, :singer, :label, :genre, :name, :price, :stock, :delivery_status)
   end
 end
