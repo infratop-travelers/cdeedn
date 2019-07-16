b357ffcd8e212fcbda14f3e76271381f2ac3f0bc
kmgk <kamegaki.shuji.infratop@gmail.com>
2019-07-16 22:36:07 +0900

    showページの雑な作成

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :items
     resources :home, only: [:index]
-    resources :customers, only: [:index]
+    resources :customers, only: [:index, :show, :destroy]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
