7f5c1867aa995da52deab7bed0e85dcc9bd9a829
kmgk <kamegaki.shuji.infratop@gmail.com>
2019-07-17 15:10:01 +0900

    issue #11

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,14 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, presence: true
+  validates :read_first_name, presence: true
+  validates :last_name, presence: true
+  validates :read_last_name, presence: true
+  validates :email, presence: true
+  validates :phone_number, presence: true
+  validates :post_code, presence: true
+  validates :prefecture, presence: true
+  validates :city, presence: true
+  validates :street, presence: true
 end
