7b295f340b34e5df42d1424d91d9db08c0491f26
kmgk <kamegaki.shuji.infratop@gmail.com>
2019-07-17 20:57:44 +0900

    アソシエーションを設定した. close #15

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+    has_many :favorites
+    has_many :discs
+    belongs_to :cart_item
+    belongs_to :order_item
+    belongs_to :singer
+    belongs_to :label
+    belongs_to :genre
 end
