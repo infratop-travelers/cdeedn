7b5eba2285263f6639720921cb3472ba94f47ede
kmgk <kamegaki.shuji.infratop@gmail.com>
2019-07-18 09:41:14 +0900

    item周りのアソシエーションがおかしかったので修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,8 @@
 class Item < ApplicationRecord
     has_many :favorites
     has_many :discs
-    belongs_to :cart_item
-    belongs_to :order_item
+    has_many :cart_item
+    has_many :order_item
     belongs_to :singer
     belongs_to :label
     belongs_to :genre
