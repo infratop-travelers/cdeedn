2f9589437ff0d7c921208e3c9b6a75ff5270f950
kamegakishuji <kamegaki.shuji.infratop@gmail.com>
2019-07-20 05:37:19 +0000

    item_kindsのルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :items
+    resources :item_kinds, only: [:index, :edit, :update, :create, :destroy]
     resources :home, only: [:index]
     resources :customers, only: [:index, :show, :edit, :update, :destroy]
     resources :resigned_customers, only: [:create,:update, :destroy]
