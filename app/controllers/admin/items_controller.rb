de8bd8f2807a0e8b50169f5cf669f151a9127af5
kmgk <kamegaki.shuji.infratop@gmail.com>
2019-07-20 22:01:17 +0900

    close #18

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,6 +2,10 @@ class Admin::ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def show
+    @item = Item.find(params[:id])
+  end
   
   def new
     @item = Item.new
