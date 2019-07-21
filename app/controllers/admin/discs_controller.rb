4380b2ebe98847c1cddb0707b2549339f144a4ad
kamegakishuji <kamegaki.shuji.infratop@gmail.com>
2019-07-21 08:36:32 +0000

    close 49

--- app/controllers/admin/discs_controller.rb
+++ app/controllers/admin/discs_controller.rb
@@ -3,7 +3,13 @@ class Admin::DiscsController < ApplicationController
     def create
         @disc = Disc.new(disc_params)
         @disc.save
-        redirect_to edit_admin_item_path(params[:item_id])
+        redirect_to edit_admin_item_path(params[:item_id]), data: {"turbolinks" => false}
+    end
+
+    def update
+        @disc = Disc.find(params[:id])
+        @disc.update(disc_params)
+        redirect_to edit_admin_item_path(params[:item_id]), data: {"turbolinks" => false}
     end
 
     def destroy
--- app/controllers/admin/songs_controller.rb
+++ app/controllers/admin/songs_controller.rb
@@ -3,7 +3,13 @@ class Admin::SongsController < ApplicationController
     def create
       @song = Song.new(song_params)
       @song.save
-      redirect_to edit_admin_item_path(params[:item_id])
+      redirect_to edit_admin_item_path(params[:item_id]), data: {"turbolinks" => false}
+    end
+
+    def update
+        @song = Song.find(params[:id])
+        @song.update(song_params)
+        redirect_to edit_admin_item_path(params[:item_id]), data: {"turbolinks" => false}
     end
 
     def destroy
