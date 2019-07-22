e415313168c378962eca31dcc37c505068f7e0f3
kmgk <kamegaki.shuji.infratop@gmail.com>
2019-07-22 09:41:22 +0900

    曲完了

--- app/controllers/admin/songs_controller.rb
+++ app/controllers/admin/songs_controller.rb
@@ -2,14 +2,37 @@ class Admin::SongsController < ApplicationController
 
     def create
       @song = Song.new(song_params)
-      @song.save
-      redirect_to edit_admin_item_path(params[:item_id]), data: {"turbolinks" => false}
+      if @song.save
+        redirect_to edit_admin_item_path(params[:item_id]), data: {"turbolinks" => false}
+      else
+        @singers = pull_down_menu(Singer.all)
+        @genres = pull_down_menu(Genre.all)
+        @labels = pull_down_menu(Label.all)
+        @item = Item.find(params[:item_id])
+        @item_discs = @item.discs.all
+        @disc = Disc.new
+
+        @song_error = "新規曲作成でのエラーです"
+        render "admin/items/edit"
+      end
     end
 
     def update
         @song = Song.find(params[:id])
-        @song.update(song_params)
-        redirect_to edit_admin_item_path(params[:item_id]), data: {"turbolinks" => false}
+        if @song.update(song_params)
+            redirect_to edit_admin_item_path(params[:item_id]), data: {"turbolinks" => false}
+        else
+            @singers = pull_down_menu(Singer.all)
+            @genres = pull_down_menu(Genre.all)
+            @labels = pull_down_menu(Label.all)
+            @item = Item.find(params[:item_id])
+            @item_discs = @item.discs.all
+            @disc = Disc.new
+
+            @song_error = "曲編集でのエラーです"
+            render "admin/items/edit"
+
+        end
     end
 
     def destroy
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,7 @@
 class Song < ApplicationRecord
     belongs_to :disc
+
+    validates :name, presence: true
+    validates :disc_id, presence: true
+    validates :number, presence: true
 end
